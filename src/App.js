019c78d6558a353334ca3a72664261178f220cec
Dave Engle <davengle@gmail.com>
2025-01-22 06:52:13 -0500

    Update Cart component to include onClose handler

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart />}
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
